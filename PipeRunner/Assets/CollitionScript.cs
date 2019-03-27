139c63dc731e419d5aeab442eea527539f826a05
unknown <sebbelebbe8930@gmail.com>
2019-03-27 10:08:31 +0100

    game

---
+++ PipeRunner/Assets/CollitionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollitionScript : MonoBehaviour
+{
+
+   
+    /*
+   private void OnCollisionEnter(Collision collision)
+  {
+       if (collision.collider.gameObject.tag == "Player") {
+           Destroy(collision.collider.gameObject);
+       }
+   }
+   */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+
+
+        {
+
+            
+            Destroy(other.gameObject);
+        }
+    }
+}
